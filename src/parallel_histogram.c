ec122d738531154517cdb933fbb71cd212d3dd17
Arthur Lima <ArthurSantos434@gmail.com>
2026-04-29 20:56:52 -0300

    add random long long data generation

--- src/parallel_histogram.c
+++ src/parallel_histogram.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <time.h>
+
+long long random_long_long(void) {
+
+    unsigned long long value = 0;
+
+    for (int i = 0; i < 5; i++)
+        value = (value << 15) ^ (unsigned long long)(rand() & 0x7FFF);
+
+    return (long long)value;
+}
+
+long long *generate_data(long long nelements) {
+
+    long long *data = malloc(nelements * sizeof(long long));
+
+    if (data == NULL)
+        return NULL;
+
+    for (long long i = 0; i < nelements; i++)
+        data[i] = random_long_long();
+
+    return data;
+}
 
 int main(int argc, char **argv) {
 
@@ -20,6 +44,22 @@ int main(int argc, char **argv) {
     printf("nbins     = %d\n", nbins);
     printf("nthreads  = %d\n", nthreads);
     printf("nr        = %d\n", nr);
+
+    srand(time(NULL));
+
+    long long *data = generate_data(nelements);
+
+    if (data == NULL) {
+        fprintf(stderr, "Error: could not allocate data array.\n");
+        return 1;
+    }
+
+    printf("First values:\n");
+    for (int i = 0; i < 5 && i < nelements; i++) {
+        printf("data[%d] = %lld\n", i, data[i]);
+    }
+
+    free(data);
 
     return 0;
 }
